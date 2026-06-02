2c29061cbd17046e43ed7632b44b5557c7f3f5be
jussray <1kaylageez@gmail.com>
2026-06-02 01:35:24 -0400

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@hooks':      './hooks',
+            '@utils':      './utils',
+            '@components': './components',
+            '@constants':  './constants',
+            '@screens':    './screens',
+            '@types':      './types',
+          },
+        },
+      ],
+    ],
   };
 };
