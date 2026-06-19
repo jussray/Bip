eec12ad851f13c01904c4f267be5511e08d04e84
Juss <286642846+jussray@users.noreply.github.com>
2026-06-19 14:13:04 -0400

    fix Pages S2Tell quick action route

--- app/(teen)/s2tell.tsx
+++ app/(teen)/s2tell.tsx
@@ -1,1 +1,5 @@
-export { default } from '../(main)/s2tell';
+import { Redirect } from 'expo-router';
+
+export default function S2TellAlias() {
+  return <Redirect href="/(teen)/bridge?compose=true" />;
+}
