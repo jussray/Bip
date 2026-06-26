dd498920cd803adb0a4c9dccbf10206c2c354348
Juss <286642846+jussray@users.noreply.github.com>
2026-06-26 09:35:44 -0400

    Add sync compatibility barrel

---
+++ src/utils/syncExports.ts
@@ -0,0 +1,4 @@
+export * from './sync';
+export * from './parentBridgeCompat';
+export { fetchPointsHistory, syncTeenActivitySummary } from './pointsCompat';
+export type { PointsHistoryEntry } from './pointsCompat';
