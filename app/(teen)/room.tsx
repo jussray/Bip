08def851110d6e5f03dd01303cc1b7251a677351
Juss <286642846+jussray@users.noreply.github.com>
2026-08-18 16:49:06 -0400

    feat(room): frame Teen Room as living sanctuary

--- app/(teen)/room.tsx
+++ app/(teen)/room.tsx
@@ -11,6 +11,7 @@ import { router } from 'expo-router';
 import { UserRoomScreen } from '@screens/UserRoomScreen';
 import { BipReturnOverlay } from '../../components/retention/BipReturnOverlay';
 import { DailyIntentionsCard } from '../../components/intentions/DailyIntentionsCard';
+import { LivingSanctuaryLayer } from '../../components/rooms/LivingSanctuaryLayer';
 import { useAppContext } from '@/context/AppContext';
 import { THEME_PACKS } from '@/constants/theme';
 import { TEEN_ROUTES } from '@/teen/routes';
@@ -80,6 +81,7 @@ export default function TeenRoomRoute() {
         sekretMode={selectedSekret}
         updateRoomMemory={updateRoomMemory}
       />
+      <LivingSanctuaryLayer companionKey={companionKey} />
       <DailyIntentionsCard
         mood={mood}
         companionKey={companionKey}
